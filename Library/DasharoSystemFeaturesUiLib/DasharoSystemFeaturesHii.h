562a92abdaf50e6d6457f765b21117fb679fa1e2
Michał Kopeć <michal.kopec@3mdeb.com>
2023-01-24 11:58:32 +0100

    Setup fan curve efi vars
    Signed-off-by: Michał Kopeć <michal.kopec@3mdeb.com>

--- Library/DasharoSystemFeaturesUiLib/DasharoSystemFeaturesHii.h
+++ Library/DasharoSystemFeaturesUiLib/DasharoSystemFeaturesHii.h
@@ -18,6 +18,7 @@ SPDX-License-Identifier: BSD-2-Clause
 #define DASHARO_USB_CONFIGURATION_FORM_ID      0x1003
 #define DASHARO_INTEL_ME_OPTIONS_FORM_ID       0x1004
 #define DASHARO_CHIPSET_CONFIGURATION_FORM_ID  0x1005
+#define DASHARO_POWER_CONFIGURATION_FORM_ID    0x1006
 
 #define DASHARO_FEATURES_DATA_VARSTORE_ID      0x0001
 
@@ -27,6 +28,34 @@ typedef struct {
   UINT16  WatchdogTimeout;
 } WATCHDOG_CONFIG;
 
+typedef struct {
+  UINT8 Temp;
+  UINT8 Duty;
+} FAN_POINT;
+
+typedef struct {
+  FAN_POINT Point1;
+  FAN_POINT Point2;
+  FAN_POINT Point3;
+  FAN_POINT Point4;
+} FAN_CURVE;
+
+#define FAN_CURVE_OPTION_SILENT 0
+#define FAN_CURVE_OPTION_PERFORMANCE 1
+
+#define FAN_CURVE_SILENT {      \
+  { .Temp = 0,   .Duty = 25  }, \
+  { .Temp = 65,  .Duty = 30  }, \
+  { .Temp = 75,  .Duty = 35  }, \
+  { .Temp = 100, .Duty = 100 }  \
+}
+
+#define FAN_CURVE_PERFORMANCE { \
+  { .Temp = 0,   .Duty = 25 },  \
+  { .Temp = 55,  .Duty = 35 },  \
+  { .Temp = 75,  .Duty = 60 },  \
+  { .Temp = 100, .Duty = 100 }  \
+}
 #pragma pack(pop)
 
 typedef struct {
@@ -36,6 +65,7 @@ typedef struct {
   BOOLEAN            ShowUsbMenu;
   BOOLEAN            ShowNetworkMenu;
   BOOLEAN            ShowChipsetMenu;
+  BOOLEAN            ShowPowerMenu;
   // Feature data
   BOOLEAN            LockBios;
   BOOLEAN            SmmBwp;
@@ -46,6 +76,8 @@ typedef struct {
   BOOLEAN            Ps2Controller;
   WATCHDOG_CONFIG    WatchdogConfig;
   BOOLEAN            WatchdogState; // holds the state of watchdog before VAR population
+  FAN_CURVE          FanCurve;
+  UINT8              FanCurveOption;
 } DASHARO_FEATURES_DATA;
 
 #define ME_MODE_ENABLE        0
@@ -56,6 +88,8 @@ typedef struct {
 #define NETWORK_BOOT_QUESTION_ID           0x8001
 #define USB_STACK_QUESTION_ID              0x8002
 #define USB_MASS_STORAGE_QUESTION_ID       0x8003
+
+
 
 extern EFI_GUID gDasharoSystemFeaturesGuid;
 
